dea7f3a4be7fa13fce68b3a27377142c13e77df2
sandeeprewa <91anu.sandeep@gmail.com>
2023-02-04 10:28:12 +0530

    Adding init

---
+++ src/main/java/com/example/demo/DddWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddWorkspaceApplication.class, args);
+	}
+
+}
